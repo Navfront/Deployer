2e7ff229e354b09f15dc77291f46a45c6ec0e2d5
Navfront <b3rt@yandex.ru>
2022-11-06 21:45:44 +0300

    fix: socket disconect event

--- src/deployer.ts
+++ src/deployer.ts
@@ -11,12 +11,12 @@ interface DeployerOptions {
 export class Deployer {
   private readonly server: SocketServer
   private readonly mem: Mem
-  private readonly connections: Socket[]
+  connections: Set<Socket>
 
   constructor (options: DeployerOptions) {
     this.server = new SocketServer({ port: options.port })
     this.mem = new Mem()
-    this.connections = []
+    this.connections = new Set()
   }
 
   private async emitAll (type: string, data: Promise<string> | string | undefined): Promise<void> {
@@ -26,20 +26,27 @@ export class Deployer {
   }
 
   async run (): Promise<void> {
+    // Deploy hand logic
     await this.server.onPath('/dploy', 'post', (req, res) => {
       const job = req.body as Job
       this.mem.pushJob(job)
       void this.emitAll('message', `${String(new Date().toISOString())} JOB: Commit: ${job.commit ?? 'undefined'} Deploy: ${job.use.join(' | ')}`).then().catch()
+
       res.status(203).send('ok')
     })
 
-    this.server.on('connect', async (socket) => {
-      this.connections.push(socket)
-      console.log(`Online: ${this.connections.length}`)
-    })
+    // On connect socket logic
+    this.server.on('connect', async (socket: Socket) => {
+      console.log(`connected id: ${socket.id}`)
 
-    this.server.on('disconnect', async () => {
-      console.log('disconnected!')
+      this.connections.add(socket)
+      console.log(`Online: ${this.connections.size}`)
+
+      socket.on('disconnect', (reason: string) => {
+        console.log(`disconected id: ${socket.id}, reason: ${reason}`)
+        this.connections.delete(socket)
+        console.log(`Online: ${this.connections.size}`)
+      })
     })
 
     this.server.run()
