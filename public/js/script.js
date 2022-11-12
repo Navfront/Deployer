257df08ee93358a79129ee3deb06e5f4bbbbb0b1
Navfront <b3rt@yandex.ru>
2022-11-12 11:21:55 +0300

    feat: add dark theme switcher

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,12 @@
 import { Logger } from "./logger.js";
 
 const root = document.querySelector("#root");
+const body = document.querySelector("body");
+const themeSwitcher = body.querySelector("#theme-switcher-input");
+themeSwitcher.addEventListener("click", () => {
+  body.classList.toggle("page-theme-dark");
+  themeSwitcher.ariaPressed = !themeSwitcher.ariaPressed;
+});
 
 const logger = new Logger(root);
 
@@ -9,7 +15,6 @@ const socket = io();
 socket.emit("hi", "images");
 
 socket.on("message", (data) => {
-  const date = new Date();
   console.log(data);
   logger.log(data);
 });
